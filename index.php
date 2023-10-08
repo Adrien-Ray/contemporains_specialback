6e81c4c0c26e58d64e12549ed88b11f8d4b91e4f
Adrien-Ray <a.raymond@codeur.online>
2023-10-08 12:41:05 +0200

    return args of url

--- index.php
+++ index.php
@@ -1,8 +1,15 @@
-<?php // silent is golden
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+require_once('./db_connect.php');
 
 $toreturn = array(
-    "key" => "value",
+    "idfrom" => (int)$_GET['idfrom'],
+    "idto" => (int)$_GET['idto'],
 );
+
+// print_r($toreturn);
 
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
